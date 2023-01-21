730893c2542465331455fbcecf8e020bae5f02e8
rajivjajo <rajivjajodia82552@gmail.com>
2023-01-21 17:33:05 +0530

    Welcome to AddressBookSystem!

---
+++ README.md
@@ -0,0 +1,3 @@
+# In this AddressBookSystem program we can add, delete , edit details of person's details like first and last names, address,
+city, state, zip, phone number and
+emailId in contacts 
---
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book system program !");
+    }
+}
